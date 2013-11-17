631708a26005ee964f63063d383d64f8945962f7
mdeibel <max.n.deibel.14@dartmouth.edu>
2013-11-17 15:31:15 -0500

    testing

--- mic_driver.js
+++ mic_driver.js
@@ -1,6 +1,10 @@
 var mic = new Microphone();
 mic.initialize();
 
+setInterval(function() {
+	document.getElementById('note').innerHTML = mic.getNote(1);		
+}, 500);
+
 /*setInterval(function() {  
     console.log(mic.getFreq(1) + ", " + mic.getNote(1));
 }, 100);*/
