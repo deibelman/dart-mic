7925b1c1e24b54cc4f14751eae206e98b56e83d1
mdeibel <max.n.deibel.14@dartmouth.edu>
2013-11-19 10:49:07 -0500

    fixing cents

--- mic_driver.js
+++ mic_driver.js
@@ -3,7 +3,7 @@ mic.initialize();
 
 setInterval(function() {
 	document.getElementById('autoNote').innerHTML = mic.getNote(1);
-	document.getElementById('autoCents').innerHTML = mic.getNoteCents(1);
+	document.getElementById('autoCents').innerHTML = mic.getNoteCents(1)[1];
 	document.getElementById('fftNote').innerHTML = mic.getNote(2);
 }, 100);
 
