b3a2109b742d4d0225814082871cb63714c0c96b
mdeibel <max.n.deibel.14@dartmouth.edu>
2013-11-19 10:52:46 -0500

    updating cents display

--- mic_driver.js
+++ mic_driver.js
@@ -5,5 +5,6 @@ setInterval(function() {
 	document.getElementById('autoNote').innerHTML = mic.getNote(1);
 	document.getElementById('autoCents').innerHTML = mic.getNoteCents(1)[1];
 	document.getElementById('fftNote').innerHTML = mic.getNote(2);
+	document.getElementById('fftCents').innerHTML = mic.getNoteCents(2)[1];
 }, 100);
 
