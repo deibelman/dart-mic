ad5e16b0104dab1766f4de860b22487be44c5f99
mdeibel <max.n.deibel.14@dartmouth.edu>
2013-11-19 10:46:29 -0500

    adding auto cents

--- mic_driver.js
+++ mic_driver.js
@@ -2,7 +2,8 @@ var mic = new Microphone();
 mic.initialize();
 
 setInterval(function() {
-	document.getElementById('autoNote').innerHTML = mic.getNote(1);	
-	document.getElementById('fftNote').innerHTML = mic.getNote(2);	
+	document.getElementById('autoNote').innerHTML = mic.getNote(1);
+	document.getElementById('autoCents').innerHTML = mic.getNoteCents(1);
+	document.getElementById('fftNote').innerHTML = mic.getNote(2);
 }, 100);
 
